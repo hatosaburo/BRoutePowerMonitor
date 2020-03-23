0e663882040bc8c0fddf357a5e8099b8001290b5
hatosaburo <yohei.fukunaga@gmail.com>
2020-03-23 11:01:24 +0900

    瞬時電力算出変更、データをJSONファイル出力に

--- main.py
+++ main.py
@@ -5,21 +5,61 @@ from __future__ import print_function
 
 import os
 import datetime
-from time import sleep
-import CommunicateSmartMeter
-import PublishAWSIoTMessage
+import time
 import schedule
+import threading
+
+import smartmeter
+import logging_util
+
+logging_util.configure_logging('logging_config_main.yaml')
+logger = logging_util.get_logger(__name__)
 
 CONFIG_FILE = 'config.yaml'
+config = None
 
-def getPowerMeasurementScheduleJob(smartMeter, awsIot):
-    smartMeter.getInstantPower(awsIot.queueInstantPowerMessage)
+lock = threading.Lock()
+wattPool = []
 
-def getIntegratePowerScheduleJob(smartMeter, awsIot):
-    smartMeter.getIntegratePower(awsIot.queueIntegratePowerMessage)
+def dumpEnergyLog(deviceId, date, power, data_dir):
+    import json
+    message = {}
+    message['device_id'] = deviceId
+    message['datetime'] = date.strftime('%Y/%m/%d %H:%M:%S')
+    message['power'] = power
 
-def publishAwsIotScheduleJob(awsIot):
-    awsIot.uploadQueueMessages()
+    record = {}
+    record['topic'] = "energy_log/notify"
+    record['message'] = message
+
+    filename = "{0}_{1}.json".format(deviceId, date.strftime('%Y-%m-%dT%H:%M:%S'))
+    filepath = os.path.join(data_dir, filename)
+
+    with open(filepath, 'w+') as f:
+        json.dump(record, f)
+
+def calcWattMinuteScheduleJob():
+    now = datetime.datetime.now()
+    now = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, 0)
+
+    lock.acquire()
+    global wattPool
+    wattMinute = round(0 if len(wattPool) == 0 else sum(wattPool) / len(wattPool), 4)
+    wattPool = []
+    lock.release()
+
+    logger.info('電力:{0}[W]'.format(wattMinute))
+    dumpEnergyLog(0, now, wattMinute, config['general']['data_dir'])
+
+def instantPowerCallback(power, date):
+    lock.acquire()
+    global wattPool
+    wattPool.append(power)
+    lock.release()
+
+def integratePowerCallback(power, date):
+    logger.info('30分電力量:{0}[kWh]'.format(power))
+    dumpEnergyLog(1, date, power, config['general']['data_dir'])    
 
 def load_config(config_path):
     import yaml
@@ -27,30 +67,26 @@ def load_config(config_path):
         return yaml.safe_load(f.read())
 
 def main():
+    global config
     config = load_config(os.path.join(os.path.dirname(__file__), CONFIG_FILE))
 
     # スマートメーターとの接続
-    smartMeter = CommunicateSmartMeter.CommunicateSmartMeter(config['general'], config['broute'])
+    smartMeter = smartmeter.smartmeter(config['general'], config['broute'], instantPowerCallback, integratePowerCallback)
     smartMeter.initialize()
     smartMeter.connect()
 
-    # AWS IoT 接続パラメータの設定
-    awsIot = PublishAWSIoTMessage.PublishAWSIoTMessage(config['awsiot'])
+    # 5秒の毎に瞬時電力を取得する
+    schedule.every(5).seconds.do(smartMeter.getInstantPower)
 
-    # 毎分00秒の電力を取得する
-    schedule.every(1).minutes.at(":00").do(getPowerMeasurementScheduleJob, smartMeter, awsIot)
+    # 毎分00秒に平均電力値を算出する
+    schedule.every(1).minutes.at(":00").do(calcWattMinuteScheduleJob)
 
-    # 取得したデータを15分ごとにAWS(DynamoDB)にアップする
-    schedule.every(10).minutes.at(":30").do(publishAwsIotScheduleJob, awsIot)
-
-    # 昨日一日分の30毎の積算電力を取得する
-    schedule.every().day.at("00:00").do(getIntegratePowerScheduleJob, smartMeter, awsIot)
+    # 昨日一日分の30毎の積算電力を取得する（取りこぼし補完目的）
+    schedule.every().day.at("04:00").do(smartMeter.getIntegratePower)
  
     while True:
         schedule.run_pending()
-        sleep(1)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
-
-
--- smartmeter.py
+++ smartmeter.py
@@ -10,6 +10,9 @@ import datetime
 import threading
 from enum import Enum
 
+import logging_util
+logger = logging_util.get_logger(__name__)
+
 class connectStatus(Enum):
     INIT = 0
     SETPASSWORD = 1
@@ -21,15 +24,17 @@ class connectStatus(Enum):
     JOINNETWORK = 7
     CONNECTED = 8
 
-class CommunicateSmartMeter():
+class smartmeter():
 
-    def __init__(self, config_general, config_broute):
+    def __init__(self, config_general, config_broute, callbackE7, callbackEA):
         self.__ser = None
         self.__rbid = config_broute['account']
         self.__rbpwd = config_broute['password']
         self.__serialPortDev = config_general['serial']
         self.__ipv6Addr = None
         self.__status = connectStatus.INIT
+        self.__callbackE7 = callbackE7
+        self.__callbackEA = callbackEA
 
     def __recvSerialPortThread(self):
         scanDuration = 4   # スキャン時間。サンプルでは6なんだけど、4でも行けるので。（ダメなら増やして再試行）
@@ -38,7 +43,7 @@ class CommunicateSmartMeter():
 
         while True:
             recvMessage = self.__ser.readline()
-            print(recvMessage, end="\r\n")
+            logger.debug(recvMessage)
 
             # \r\nで終わるまで継続して受信データを待つ
             # readline()が\nで一行として区切るため、バイナリデータ部に\nのデータが含まれると分割されてしまう
@@ -61,6 +66,7 @@ class CommunicateSmartMeter():
                     self.__status = connectStatus.SETACCOUNT
 
                 elif self.__status == connectStatus.SETACCOUNT:
+                    logger.info(u"スキャン実行中...")
                     scanDuration = 4   # スキャン時間。サンプルでは6なんだけど、4でも行けるので。（ダメなら増やして再試行）
                     strCommand = "SKSCAN 2 FFFFFFFF " + str(scanDuration) + "\r\n"
                     self.__sendCommand(strCommand.encode(encoding='utf-8'))
@@ -83,9 +89,11 @@ class CommunicateSmartMeter():
                 elif recvMessage.startswith(b"EVENT 22"):
                     # スキャン完了 
                     if b"Channel" in scanRes:
+                        logger.info(u"スキャン完了、接続中...")
                         self.__sendCommand(b"SKSREG S2 " + scanRes[b"Channel"] + b"\r\n")
                         self.__status = connectStatus.SETCHANNEL
                     else:
+                        logger.info(u"スキャン再試行中...")
                         scanDuration += 1
                         strCommand = "SKSCAN 2 FFFFFFFF " + str(scanDuration) + "\r\n"
                         self.__sendCommand(strCommand.encode(encoding='utf-8'))
@@ -97,11 +105,12 @@ class CommunicateSmartMeter():
 
             elif self.__status == connectStatus.JOINNETWORK:
                 if recvMessage.startswith(b"EVENT 24") :
-                    print("PANA 接続失敗..接続再試行")
+                    logger.info("PANA 接続失敗..接続再試行")
                     self.__status = connectStatus.INIT
                     self.connect()
                     
                 elif recvMessage.startswith(b"EVENT 25") :
+                    logger.info(u"接続完了！")
                     self.__status = connectStatus.CONNECTED
                     # 積算電力量の単位を取得する
                     self.__requestPropertyRW(b"\xE1")
@@ -121,7 +130,7 @@ class CommunicateSmartMeter():
             EPC = data[0:0+1]
             length = data[1]
             if not len(data[2:]) >= length:
-                print(u"不完全データは破棄:{0}".format(data))
+                logger.warning(u"不完全データは破棄:{0}".format(data))
                 break
             
             if EPC == b"\xE1":
@@ -129,32 +138,42 @@ class CommunicateSmartMeter():
                 # 積算電力収集日を設定する
                 self.__requestPropertyRW(b"\xE5", read=False, data=(1).to_bytes(1, 'big'))
             elif EPC == b"\xE2":
-                # 前日を日付を設定
+                # 30分電力量履歴
+
+                # 何日前か、から日付を設定
                 date = datetime.datetime.now().replace(hour=0,minute=0,second=0,microsecond=0) - datetime.timedelta(days=int.from_bytes(data[2:2+2], 'big'))
 
                 # 30分値電力を取得
                 data_powers = data[4:]
-                powers = []
                 while len(data_powers) > 0:
-                    powers.append(int.from_bytes(data_powers[0:0+4], 'big'))
+                    power = int.from_bytes(data_powers[0:0+4], 'big')
+
+                    # 未計測値移行はスキップ
+                    if power == 0xFFFFFFFE:
+                        break
+
+                    power = self.__convertIntegratePowerUnit(power)
+                    self.__callbackEA(power, date)
+                    logger.info(u"積算電力履歴(IN):{0} {1}".format(date, power))
+
                     data_powers = data_powers[4:]
-                print(u"積算電力履歴(IN):{0} {1}".format(date, powers))
-                self.__callbackE2(powers, date)
+                    date += datetime.timedelta(minutes=30)
             elif EPC == b"\xE5":
                 pass
             elif EPC == b"\xE7":
                 # 瞬時電力計測値
                 power = int.from_bytes(data[2:2+4], 'big')
-                print(u"{0}:瞬時電力計測値:{1}[W] ".format(self.__requestTime, power))
-                self.__callbackE7(power, self.__requestTime)
+                logger.debug(u"瞬時電力計測値:{0}[W] ".format(power))
+                self.__callbackE7(power, datetime.datetime.now())
             elif EPC == b"\xEA":
                 # 30分電力積算量(IN)
-                year, month, day, hour, minute, second, power = int.from_bytes(data[2:2+2], 'big'), data[4], data[5], data[6], data[7], data[8], int.from_bytes(data[9:9+4], 'big')
-                print(u"30分電力積算量(IN):{0}/{1}/{2} {3}:{4}:{5} {6}kWh".format(year, month, day, hour, minute, second, power))
+                year, month, day, hour, minute, second, power = int.from_bytes(data[2:2+2], 'big'), data[4], data[5], data[6], data[7], data[8], self.__convertIntegratePowerUnit(int.from_bytes(data[9:9+4], 'big'))
+                logger.info(u"30分電力積算量(IN):{0}[kWh]".format(power))
+                self.__callbackEA(power, datetime.datetime(year, month, day, hour, minute, second))
             elif EPC == b"\xEB":
                 # 30分電力積算量(OUT)
-                year, month, day, hour, minute, second, power = int.from_bytes(data[2:2+2], 'big'), data[4], data[5], data[6], data[7], data[8], int.from_bytes(data[9:9+4], 'big')
-                print(u"30分電力積算量(OUT):{0}/{1}/{2} {3}:{4}:{5} {6}kWh".format(year, month, day, hour, minute, second, power))
+                year, month, day, hour, minute, second, power = int.from_bytes(data[2:2+2], 'big'), data[4], data[5], data[6], data[7], data[8], self.__convertIntegratePowerUnit(int.from_bytes(data[9:9+4], 'big'))
+                logger.info(u"30分電力積算量(OUT):{0}[kWh]".format(power))
 
             data = data[1 + 1 + length:]
 
@@ -170,18 +189,16 @@ class CommunicateSmartMeter():
         self.__recvThread.start()
 
     def connect(self):
+        logger.info(u"スマートメーター接続開始")
         self.__sendCommand(b"SKSETPWD C " + self.__rbpwd.encode('utf-8') + b"\r\n")
         self.__status = connectStatus.SETPASSWORD
 
-    def getIntegratePower(self, callback):
+    def getIntegratePower(self):
         """積算消費電力量を取得します"""
-        self.__callbackE2 = callback
         return self.__requestPropertyRW(b"\xE2")
 
-    def getInstantPower(self, callback):
+    def getInstantPower(self):
         """瞬時電力消費量を取得します"""
-        self.__requestTime = datetime.datetime.now()
-        self.__callbackE7 = callback
         return self.__requestPropertyRW(b"\xE7")
 
     def __requestPropertyRW(self, epc, read=True, data=b""):
@@ -205,6 +222,30 @@ class CommunicateSmartMeter():
         self.__sendCommand(command)
 
         return 0
+
+    def __convertIntegratePowerUnit(self, sourcePower):
+        rate = 1
+
+        if self.__unit == 0x00:
+            rate = 1
+        elif self.__unit == 0x01:
+            rate = 0.1
+        elif self.__unit == 0x02:
+            rate = 0.01
+        elif self.__unit == 0x03:
+            rate = 0.001
+        elif self.__unit == 0x04:
+            rate = 0.0001
+        elif self.__unit == 0x0A:
+            rate = 10
+        elif self.__unit == 0x0B:
+            rate = 100
+        elif self.__unit == 0x0C:
+            rate = 1000
+        elif self.__unit == 0x0D:
+            rate = 10000 
+        
+        return round(sourcePower * rate, 4)
 
     def close(self,) :
         self.__ser.close()
